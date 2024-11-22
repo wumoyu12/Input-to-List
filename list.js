6e0e4111d5262fbf7a0c7f76a038480386930c64
wumoyu12 <147264549+wumoyu12@users.noreply.github.com>
2024-11-22 10:08:53 -0500

    Update list.js

--- list.js
+++ list.js
@@ -1,1 +1,29 @@
+window.addEventListener("load", addListener);
 
+function addListener()
+{
+    /*document.getElementById("which").style.display = "block";
+    document.getElementById("ol").style.display = "none";
+	/*document.getElementById("ul").style.display = "none";*/
+	document.getElementById("btnol").addEventListener("click", OrderList);
+    document.getElementById("btnul").addEventListener("click", UnorderedList);
+}
+
+function OrderList()
+{
+	/*document.getElementById("which").style.display = "none";
+	document.getElementById("ol").style.display = "block";*/
+	GetItem()
+}
+
+function UnorderedList()
+{
+	/*document.getElementById("which").style.display = "none";
+	document.getElementById("ul").style.display = "block";*/
+	GetItem()
+}
+
+function GetItem()
+{
+	num = document.getElementById("txtnum").value;
+}

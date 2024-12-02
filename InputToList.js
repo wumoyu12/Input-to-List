a24fdf67b5439f99c5c0ce56d91879bbb2fcbfa0
wumoyu12 <147264549+wumoyu12@users.noreply.github.com>
2024-12-02 13:46:53 -0500

    Update InputToList.js

--- InputToList.js
+++ InputToList.js
@@ -4,61 +4,61 @@ let items = [];
 
 function addListener()
 {
-  document.getElementById("btnadd").addEventListener("click", Add);
-  document.getElementById("btndisplay").addEventListener("click", Display);
+	document.getElementById("btnadd").addEventListener("click", Add);
+	document.getElementById("btndisplay").addEventListener("click", Display);
 }
 
 function Add()
 {
-  let item = document.getElementById("txtitem").value;
+	let item = document.getElementById("txtitem").value;
+	
+	if (CheckItem(item))
+	{
+		items.push(item);
+		alert("Added: " + item);
+	}
 
-  if (CheckItem(item))
-  {
-    items.push(item);
-    alert("Added: " + item);
-  }
-
-  document.getElementById("txtitem").value = "";
+	document.getElementById("txtitem").value = "";
 }
 
 function CheckItem(item)
 {
-  if (item == "")
-  {
-    alert("You have to enter something");
-    return false;
-  }
-
-  for (let i = 0; i < items.length; i++)
-  {
-    if (items[i] == item)
-    {
-      alert("You've already entered : " + item + ", please enter something else");
-      return false;
-    }
-  }
-
-  return true;
+	if (item == "")
+	{
+		alert("You have to enter something");
+		return false;
+	}
+	
+	for (let i = 0; i < items.length; i++)
+	{
+		if (items[i] == item)
+		{
+			alert("You've already entered : " + item + ", please enter something else");
+			return false;
+		}
+	}
+	
+	return true;
 }
 
 function Display()
 {
-  if (items.length < 2)
-  {
-    alert("You need to enter at least two things before displaying the list.");
-    return;
-  }
+	if (items.length < 2)
+	{
+		alert("You need to enter at least two things before displaying the list.");
+		return;
+	}
 
-  document.getElementById("Ask").style.display = "none";
-  document.getElementById("display").style.display = "block";
-
-  let list = document.getElementById("list");
-  list.innerHTML = "";
-
-  for (let i = 0; i < items.length; i++)
-  {
-    let li = document.createElement("li");
-    li.innerText = items[i];
-    list.appendChild(li);
-  }
+	document.getElementById("Ask").style.display = "none";
+	document.getElementById("display").style.display = "block";
+	
+	let list = document.getElementById("list");
+	list.innerHTML = "";
+	
+	for (let i = 0; i < items.length; i++)
+	{
+		let li = document.createElement("li");
+		li.innerText = items[i];
+		list.appendChild(li);
+	}
 }
